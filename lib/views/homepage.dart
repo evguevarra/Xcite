5d2902e1864f2efb989d5706353771a60f8f40f0
evguevarra <evguevarra08@gmail.com>
2021-09-27 16:50:18 +0800

    Splashscreen added

---
+++ lib/views/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomPageState createState() => _HomPageState();
+}
+
+class _HomPageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
